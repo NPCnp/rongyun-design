b3c8e397b67ee89fb6c67a52931ef9285309ac99
NPCnp <15759568956@139.com>
2026-05-27 23:16:57 +0800

    修改订单号生成逻辑，从Supabase读取最大订单号，没有则从1开始

--- standalone/js/app.js
+++ standalone/js/app.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2fa5d2ce9d550d30111536c04e297efdcd87c05138b3519b6d1e491f372694e8
-size 38410
+oid sha256:9a71eb7bd50fbc61dd5b4fedb932c7eeda71de8fff2d9e1851f2bb54799c6cc9
+size 38761
